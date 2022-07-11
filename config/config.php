ce143d7127594a87fe8354b087801b4c7401923f
AmirAli <echoamirali@gmail.com>
2022-07-11 17:10:41 +0430

    added some options to config file

--- config/config.php
+++ config/config.php
@@ -4,5 +4,10 @@
  * You can place your custom package configuration in here.
  */
 return [
+    'tranlate' => true,
+    'translate_from' => 'fa',
+    'translate_to' => 'en',
+    'field' => 'slug',
+    'pattern' => '',
 
 ];
