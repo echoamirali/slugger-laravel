701d30ebc2c78e9e13fa1adbd7d0f46c57dd2f22
AmirAli <echoamirali@gmail.com>
2022-07-12 23:02:26 +0430

    did some changes in main file

--- src/Slugger.php
+++ src/Slugger.php
@@ -2,13 +2,26 @@
 
 namespace Echoamirali\Slugger;
 
+use Echoamirali\Slugger\Classes\GoogleTranslate;
+
 class Slugger
 {
     public function do_initial($slug)
     {
+        $slug = self::do_translate($slug);
+        $slug = trim($slug);
         $slug = preg_replace('/\s+/', '-', $slug);
         $slug = strtolower($slug);
+        return $slug;
     }
 
+    public static function do_translate($slug)
+    {
+        $translator = new GoogleTranslate();
+            
+        $slug = $translator->translate('en', 'fa', $slug);
+
+        return $slug;
+    }
     
 }
