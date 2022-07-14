9f14427dcb9ded57491cf7866eec22f7f443ca23
AmirAli <echoamirali@gmail.com>
2022-07-14 12:50:38 +0430

    modified symbol class

--- src/Classes/Symbols.php
+++ src/Classes/Symbols.php
@@ -3,5 +3,25 @@
 namespace Echoamirali\Slugger\Classes;
 
 class Symbols {
-    
+
+    protected $range;
+
+    public function __construct($symbol)
+    {
+        switch($symbol):
+            case 'decimal':
+                $this->range = range(1,99);
+                break;
+            case 'decimal_leading_zero':
+                $this->range = $this->fillDecimalLeadingZero();
+        endswitch;
+
+    }
+
+    protected function fillDecimalLeadingZero()
+    {
+        foreach(range(1, 99) as $number):
+
+        endforeach;
+    }
 }
