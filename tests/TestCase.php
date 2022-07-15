c9ff3470341ad676d6b872dda01d51a82c7f4885
AmirAli <echoamirali@gmail.com>
2022-07-15 11:25:42 +0430

    modified testcase

---
+++ tests/TestCase.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Echoamirali\Slugger\Tests;
+
+use Echoamirali\Slugger\SluggerServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+class TestCase extends Orchestra
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+}
