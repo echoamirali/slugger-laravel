d141e1fe383af74d1b5abb71153efc24181389f0
AmirAli <echoamirali@gmail.com>
2022-07-16 17:30:51 +0430

    added roman optin to symbol class

--- src/Classes/Symbols.php
+++ src/Classes/Symbols.php
@@ -13,15 +13,36 @@ class Symbols {
                 $this->range = range(1,99);
                 break;
             case 'decimal_leading_zero':
-                $this->range = array_map(function($val){
-                    return sprintf('%03d', $val);
-                }, range(1,99));
+                $this->range = array_map([get_called_class(), 'addLeadingZero'], range(1,99));
                 break;
             case 'alphabet':
                 $this->range = range('a', 'z');
                 break;
+            case 'roman':
+                $this->range = array_map([get_called_class(), 'numberToRoman'], range(1,99));
+                break;
 
         endswitch;
+    }
+
+    public function numberToRoman($number) {
+        $map = ['XC' => 90, 'L' => 50, 'XL' => 40, 'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1];
+        $returnValue = '';
+        while ($number > 0) {
+            foreach ($map as $roman => $int) {
+                if($number >= $int) {
+                    $number -= $int;
+                    $returnValue .= $roman;
+                    break;
+                }
+            }
+        }
+        return strtolower($returnValue);
+    }
+
+    public function addLeadingZero($number)
+    {
+        return sprintf('%03d', $number);
     }
 
     public function get()
