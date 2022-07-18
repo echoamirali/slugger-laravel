418ff5be5e16fa86980ec73a2ef3308cbd32a08c
AmirAli <echoamirali@gmail.com>
2022-07-18 17:30:21 +0430

    changed some methods

--- src/Classes/Symbols.php
+++ src/Classes/Symbols.php
@@ -2,6 +2,8 @@
 
 namespace Echoamirali\Slugger\Classes;
 
+use NumberFormatter;
+
 class Symbols {
 
     protected $range;
@@ -13,16 +15,13 @@ class Symbols {
                 $this->range = range(1,99);
                 break;
             case 'decimal_leading_zero':
-                $this->range = array_map([get_called_class(), 'addLeadingZero'], range(1,99));
+                $this->range = array_map([get_called_class(), 'toLeadingZero'], range(1,99));
                 break;
             case 'roman':
-                $this->range = array_map([get_called_class(), 'numberToRoman'], range(1,99));
-                break;
-            case 'cardinal_numbers':
-                $this->range = $this->fillCardinalNumbers();
+                $this->range = array_map([get_called_class(), 'toRoman'], range(1,99));
                 break;
             case 'ordinal_numbers':
-                $this->range = array_map([get_called_class(), 'ordinalNumber'], range(1,99));
+                $this->range = array_map([get_called_class(), 'toOrdinal'], range(1,99));
                 break;
             default:
                 if( in_array($symbol, array_keys(config('slugger.custom_iteration_symbols'))) )
@@ -33,14 +32,14 @@ class Symbols {
         endswitch;
     }
 
-    public function ordinalNumber($number)
+    public function toOrdinal($number)
     {
         $locale = 'en_US';
-        $nf = new \NumberFormatter($locale, \NumberFormatter::ORDINAL);
+        $nf = new NumberFormatter($locale, NumberFormatter::ORDINAL);
         return $nf->format($number);
     }
 
-    public function numberToRoman($number) {
+    public function toRoman($number) {
         $map = ['XC' => 90, 'L' => 50, 'XL' => 40, 'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1];
         $returnValue = '';
         while ($number > 0) {
@@ -55,23 +54,9 @@ class Symbols {
         return strtolower($returnValue);
     }
 
-    public function addLeadingZero($number)
+    public function toLeadingZero($number)
     {
         return sprintf('%03d', $number);
-    }
-
-    public function fillCardinalNumbers()
-    {
-        return ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
-        "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty",
-        "twenty-one", "twenty-two", "twenty-three", "twenty-four", "twenty-five", "twenty-six", "twenty-seven", "twenty-eight", "twenty-nine", "thirty",
-        "thirty-one", "thirty-two", "thirty-three", "thirty-four", "thirty-five", "thirty-six", "thirty-seven", "thirty-eight", "thirty-nine", "forty",
-        "forty-one", "forty-two", "forty-three", "forty-four", "forty-five", "forty-six", "forty-seven", "forty-eight", "forty-nine", "fifty",
-        "fifty-one", "fifty-two", "fifty-three", "fifty-four", "fifty-five", "fifty-six", "fifty-seven", "fifty-eight", "fifty-nine", "sixty",
-        "sixty-one", "sixty-two", "sixty-three", "sixty-four", "sixty-five", "sixty-six", "sixty-seven", "sixty-eight", "sixty-nine", "seventy",
-        "seventy-one", "seventy-two", "seventy-three", "seventy-four", "seventy-five", "seventy-six", "seventy-seven", "seventy-eight", "seventy-nine", "eighty",
-        "eighty-one", "eighty-two", "eighty-three", "eighty-four", "eighty-five", "eighty-six", "eighty-seven", "eighty-eight", "eighty-nine", "ninety",
-        "ninety-one", "ninety-two", "ninety-three", "ninety-four", "ninety-five", "ninety-six", "ninety-seven", "ninety-eight", "ninety-nine"];
     }
 
     public function get()
