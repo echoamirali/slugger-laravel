ea09628686a55ec3b38c4d303147e3c3759240e4
AmirAli <echoamirali@gmail.com>
2022-07-19 17:39:47 +0430

    added a new test

---
+++ tests/Feature/CreateArticleTest.php
@@ -0,0 +1,23 @@
+<?php 
+
+namespace Echoamirali\Slugger\Tests\Feature\CreateArticleTest;
+
+use Echoamirali\Slugger\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Echoamirali\Slugger\Tests\Article;
+
+class CreateArticleTest extends TestCase {
+
+    use RefreshDatabase;
+
+    /** @test */
+    function create_new_article()
+    {
+        $article = Article::create([
+            'title' => 'title',
+            'slug' => 'slug'
+        ]);
+
+        $this->assertEquals('slug-1', $article);
+    }
+}
