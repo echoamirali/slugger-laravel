dd6ac3746c9fb35cdfc3547c5972f9c049a6b408
AmirAli <echoamirali@gmail.com>
2022-07-19 19:46:10 +0430

    modified article model

--- tests/Article.php
+++ tests/Article.php
@@ -1,11 +1,16 @@
 <?php
 
-namespace Echoamirali\Slugger\Tests\Article;
+namespace Echoamirali\Slugger\Tests;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model {
 
     protected $table = 'articles';
+    public $timestamps = false;
+    protected $fillable = [
+        'title',
+        'slug'
+    ];
 
 }
