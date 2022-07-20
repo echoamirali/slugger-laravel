2c39ddf454ab021bc861e6f63fb0fb75056793af
AmirAli <echoamirali@gmail.com>
2022-07-20 17:00:34 +0430

    added sluger trait to article model

--- tests/Article.php
+++ tests/Article.php
@@ -3,8 +3,11 @@
 namespace Echoamirali\Slugger\Tests;
 
 use Illuminate\Database\Eloquent\Model;
+use Echoamirali\Slugger\Traits\Slugger;
 
 class Article extends Model {
+
+    use Slugger;
 
     protected $table = 'articles';
     public $timestamps = false;
