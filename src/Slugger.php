6785893439da8ba05ab78083e7035d5863af2866
AmirAli <echoamirali@gmail.com>
2022-07-22 17:54:10 +0430

    modified methods

--- src/Slugger.php
+++ src/Slugger.php
@@ -17,7 +17,8 @@ class Slugger
     public static function doTranslate($string, $from, $to)
     {
         $translator = new GoogleTranslate();
-            
+        $from = empty($from) ? 'en' : $from;
+        $to = empty($to) ? 'en' : $to;
         $string = $translator->translate($from, $to, $string);
 
         return $string;
@@ -32,8 +33,11 @@ class Slugger
     public static function make($string, $from_config = true, $config_options = null)
     {
         $config = $from_config ? config('slugger') : $config_options;
+        if( isset($config['do_translate'], $config['translate_from'], $config['translate_to']) && $config['do_translate'] )
+            $string = self::doTranslate($string, $config['translate_from'], $config['translate_to']);
         if( isset($config['do_initial']) && $config['do_initial'] )
             $string = self::doInitial($string);
+
         return $string;
     }
 }
