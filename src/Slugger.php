4cba3400162e4dad9f934114484140fdae7364ec
AmirAli <echoamirali@gmail.com>
2022-07-23 18:43:12 +0430

    modified do pattern in make method

--- src/Slugger.php
+++ src/Slugger.php
@@ -37,8 +37,8 @@ class Slugger
             $string = self::doTranslate($string, $config['translate_from'], $config['translate_to']);
         if( isset($config['do_initial']) && $config['do_initial'] )
             $string = self::doInitial($string);
-        if( isset($config['do_pattern']) && $config['do_pattern'] )
-            $string = self::doPattern($string);
+        if( isset($config['do_pattern'], $config['pattern']) && $config['do_pattern'])
+            $string = self::doPattern($string, $config['pattern']);
         return $string;
     }
 }
