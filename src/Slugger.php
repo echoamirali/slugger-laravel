69d86749c9aac4b24fd7c4e87e5c45052df97224
AmirAli <echoamirali@gmail.com>
2022-07-24 21:22:04 +0430

    solved problems

--- src/Slugger.php
+++ src/Slugger.php
@@ -36,29 +36,28 @@ class Slugger
     {
         switch($config_status):
             case 'config_mixed':
-                $config = array_merge(config('slugger'), $config_options);
+                $this->config = array_merge(config('slugger'), $config_options);
                 break;
             case 'config_options':
-                $config = $config_options;
+                $this->config = $config_options;
                 break;
             case 'config_file':
             default:
-                $config = config('slugger');
+                $this->config = config('slugger');
                 break;
         endswitch;
-        return $config;
     }
 
     // config status options = config_file, config_options, config_mixed
     public static function make($string, $config_status = 'config_file' , $config_options = null)
     {
-        $config = $this->prepareConfig($config_status, $config_options);
-        if( isset($config['do_translate'], $config['translate_from'], $config['translate_to']) && $config['do_translate'] )
-            $string = self::doTranslate($string, $config['translate_from'], $config['translate_to']);
-        if( isset($config['do_initial']) && $config['do_initial'] )
+        self::prepareConfig($config_status, $config_options);
+        if( isset($this->config['do_translate'], $this->config['translate_from'], $this->config['translate_to']) && $this->config['do_translate'] )
+            $string = self::doTranslate($string, $this->config['translate_from'], $this->config['translate_to']);
+        if( isset($this->config['do_initial']) && $this->config['do_initial'] )
             $string = self::doInitial($string);
-        if( isset($config['do_pattern'], $config['pattern']) && $config['do_pattern'])
-            $string = self::doPattern($config['pattern'], $string);
+        if( isset($this->config['do_pattern'], $this->config['pattern']) && $this->config['do_pattern'])
+            $string = self::doPattern($this->config['pattern'], $string);
         return $string;
     }
 }
