45d508a43f69618f2d0196d627fbe63ab7bd7787
AmirAli <echoamirali@gmail.com>
2022-07-25 14:04:18 +0430

    modified slugger trait

--- src/Traits/Slugger.php
+++ src/Traits/Slugger.php
@@ -1,12 +1,18 @@
 <?php
 
 namespace Echoamirali\Slugger\Traits;
+use Echoamirali\Slugger\Slugger as MainSlugger;
 
 class Slugger {
     
+    public function setSlugAttribute($value)
+    {
+        $this->attributes['slug'] = strtolower($value);
+    }
+
     public function make()
     {
-        
+        MainSlugger::make();
     }
 
 }
