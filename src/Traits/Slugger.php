a1d38d4d0b7a55ec374e31ad9710916897eae334
AmirAli <echoamirali@gmail.com>
2022-08-01 17:35:27 +0430

    modified slugger trait

--- src/Traits/Slugger.php
+++ src/Traits/Slugger.php
@@ -9,7 +9,7 @@ trait Slugger {
 
     public function setAttribute($key, $value)
     {
-        if( $key == config('slugger.field') )
+        if( $key == $this->slugger_config['field'] )
             return parent::setAttribute($key, $this->make($value));
         return parent::setAttribute($key, $value);
     }
@@ -19,7 +19,7 @@ trait Slugger {
         $index = 1;
         $symbols = MainSlugger::symbolsArray();
         $raw_slug = $slug;
-        while ( $this->query()->where(config('slugger.field'), $slug)->exists() ) {
+        while ( $this->query()->where($this->slugger_config['field'], $slug)->exists() ) {
             $symbol = isset($symbols[$index]) ? $symbols[$index] : $index;
             $slug = $raw_slug."-".$symbol;
             $index++;
@@ -29,8 +29,10 @@ trait Slugger {
 
     public function make($string)
     {
+        $config = $this->slugger_config;
+        $this->slugger_config = array_merge(config('slugger'), $config);
         $slug = MainSlugger::make($string);
-        if( config('slugger.is_unique') )
+        if( $this->slugger_config['is_unique'] )
             return $this->makeUnique($slug);
         return $slug;
     }
